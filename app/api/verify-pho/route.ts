957d0f09ff2556ff320bd612844bfaa3aba681a6
refsilva <refsilva@yahoo.com>
2025-09-03 15:13:27 +0100

    added new 11

--- app/api/verify-pho/route.ts
+++ app/api/verify-pho/route.ts
@@ -88,6 +88,7 @@ export async function POST(request: Request) {
       success: true,
       data: response.data,
     });
+     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     console.error("Smile ID API Error:", error.response?.data || error.message);
 
